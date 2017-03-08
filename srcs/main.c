2b60da9623a045433f76498729243122662575d8
Jean JACOBI <jjacobi@student.42.fr>
2017-03-07 20:30:51 -0800

    Working on it

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,23 @@
 #include "libft.h"
 #include "ft_printf.h"
+#include "get_next_line.h"
+#include <fcntl.h>
 
 int	main(void)
 {
+	int		i;
+	int		j;
+	int		z;
+	char	*str[99];
+
+	i = 1;
+	j = 0;
+	while (i)
+		i = get_next_line(1, &str[i - 1]);
+	j = open("./test.log", O_RDWR);
+	z = i;
+	while (i) {
+		ft_putendl_fd(str[z - i--], j);
+	}
 	return (0);
 }
